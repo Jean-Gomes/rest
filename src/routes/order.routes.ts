8c68a87893cdad7ce72cfb2d085f8a47185734c5
Jean-Gomes <jgs.bha@gmail.com>
2026-02-18 21:17:12 -0300

    refactor: added resources in order API

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express';
 import { createOrderService } from '../services/order.service';
+import { Resource, ResourceCollection } from '../http/resources';
 
 const router = Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     const orderService = await createOrderService();
     const { payment_method, card_token, cart_uuid } = req.body;
     // @ts-expect-error
     const customerId = req.userId;
     const { order, payment } = await orderService.createOrder({customerId, payment_method, card_token, cart_uuid});
-    res.json({ order, payment });
+    
+    const resource = new Resource({ order, payment });
+    next(resource)
 });
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     const orderService = await createOrderService();
     const { page = 1, limit = 10 } = req.query;
     // @ts-expect-error
@@ -22,7 +25,17 @@ router.get('/', async (req, res) => {
         limit: parseInt(limit as string),
         customerId
     });
-    res.json({ orders, total });
+
+
+    const collection = new ResourceCollection(orders, {
+            paginationData: {
+            total,
+            page: parseInt(page as string),
+            limit: parseInt(limit as string),
+        },
+    });
+
+    return next(collection);
 });
 
 
